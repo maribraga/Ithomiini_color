9289c4df9332727064c72d0bcf8a2f4e6a9c6bfa
Mariana P Braga <mpiresbr@gmail.com>
2021-10-15 10:50:10 +0300

    Add data and code folders

---
+++ code/batch.sh
@@ -0,0 +1,6 @@
+tmux new-session -d -s "itho-1" ./run_inf.sh Itho_inf.Rev 1;sleep 0.1
+tmux new-session -d -s "itho-2" ./run_inf.sh Itho_inf.Rev 2;sleep 0.1
+tmux new-session -d -s "itho-3" ./run_inf.sh Itho_inf.Rev 3;sleep 0.1
+tmux new-session -d -s "itho-4" ./run_inf.sh Itho_inf.Rev 4;sleep 0.1
+tmux new-session -d -s "itho-5" ./run_inf.sh Itho_inf.Rev 5;sleep 0.1
+tmux new-session -d -s "itho-6" ./run_inf.sh Itho_inf.Rev 6;sleep 0.1
---
+++ code/data_exp_and_prep.R
@@ -0,0 +1,52 @@
+# Exploring the data before inference in RevBayes
+
+library(ape)
+library(tidyverse)
+library(ggtree)
+
+tree <- read.tree("./data/itho_phylo.tre")
+host_tree <- read.tree("./data/44tips.tre")
+data <- read.nexus.data("./data/original_Ithomiini_mimicry_data_mtx.nex") %>% 
+  as.data.frame() %>% 
+  apply(2, as.numeric) %>% 
+  t()
+colnames(data) <- paste0("C",1:44)
+
+ggt <- ggtree(tree, layout = 'circular') + geom_tiplab(size = 1) + theme_tree2()
+gheatmap(ggt, data)
+
+# histogram
+
+common <- data %>%
+  as_tibble() %>% 
+  mutate(across(everything(), sum)) %>% 
+  slice_head() %>% 
+  pivot_longer(everything(),names_to = "color", values_to = "n_spp") %>% 
+  arrange(desc(n_spp))
+
+plot_spp_rich <- ggplot(common) + 
+  geom_col(aes(reorder(color, desc(n_spp)),n_spp)) + 
+  labs(x = "Color patterns", y = "Species richness") +
+  #geom_hline(yintercept = 10, col = "red") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 270))
+
+ggplot(common) + geom_density(aes(n_spp)) + theme_bw()
+
+
+col_div <- t(data) %>%
+  as_tibble() %>% 
+  mutate(across(everything(), sum)) %>% 
+  slice_head() %>% 
+  pivot_longer(everything(),names_to = "spp", values_to = "n_col")
+
+plot_col_div <- ggplot(col_div) + 
+  geom_col(aes(reorder(spp, desc(n_col)),n_col)) + 
+  labs(x = "Number of color patterns", y = "Species") +
+  #geom_hline(yintercept = 10, col = "red") +
+  theme_bw() +
+  theme(axis.text.x = element_blank())
+
+
+ggplot(col_div) + geom_density(aes(n_col)) + theme_bw()
+
